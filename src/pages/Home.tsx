2c670a3964c4a360920e9053039e45235ad5966a
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2026-01-22 16:21:24 +0000

    refactor: implement react-router-dom and extract home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+// src/pages/Home.tsx
+import Hero from '../features/hero/Hero';
+import ProjectList from '../features/projects/ProjectList';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <ProjectList />
+    </>
+  );
+};
+
+export default Home;
