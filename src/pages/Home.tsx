c1d9b78ed1c733cd89f7e88b1e7c665e31ef8336
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2026-02-19 20:56:56 +0000

    fix: open links on a new tab

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Hero from '../features/hero/Hero';
 import ProjectList from '../features/projects/ProjectList';
 import AboutMe from '../features/about/AboutMe'; 
 import SkillsGrid from '../features/skills/SkillsGrid'; // ⬅️ NEW IMPORT
 
 const Home = () => {
+  const { hash } = useLocation();
+  useEffect(() => {
+    if (hash) {
+      const element = document.getElementById(hash.replace('#', ''));
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }, 100);
+      }
+    }
+  }, [hash]);
   return (
     <>
       <Hero />
