099a253edf726f1fb92ec909db00c456b8ceed83
mikiyas-stp <mikiyasgebremichaell@gmail.com>
2026-02-19 21:12:35 +0000

    scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Layout from './components/layout/Layout';
 import Home from './pages/Home';
 import ProjectDetails from './pages/ProjectDetails';
 import NotFound from './pages/NotFound';
+import ScrollToTop from './components/common/ScrollToTop';
 
 function App() {
   return (
     <div className="min-h-screen"> 
+    <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
